26090ea72592ca1b2bca9519a81b7a7ce2f91d8d
Gabriela <gabrielaaa1413@gmail.com>
2019-09-13 20:00:10 -0500

    [resolves #4] Frontend list calls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SolicitudEstudianteComponent } from './solicitudes/solicitud-estudiante/solicitud-estudiante.component';
 import { InicioPracticaComponent } from './practicas/inicio-practica/inicio-practica.component';
+import { ConvocatoriasListarComponent } from 'src/app/convocatorias/convocatorias-listar/convocatorias-listar.component';
 import { ConvocatoriasInscripcionComponent } from 'src/app/convocatorias/convocatorias-inscripcion/convocatorias-inscripcion.component';
 import { ConvocatoriasModificarComponent } from 'src/app/convocatorias/convocatorias-modificar/convocatorias-modificar.component';
 
@@ -9,7 +10,8 @@ const routes: Routes = [
   { path : 'inscripcion', component : ConvocatoriasInscripcionComponent },
   { path : 'solicitud/estudiante', component : SolicitudEstudianteComponent },
   { path : 'modificar', component: ConvocatoriasModificarComponent },
-  { path : 'inicio-practica', component : InicioPracticaComponent }
+  { path : 'inicio-practica', component : InicioPracticaComponent },
+  { path: 'listar', component: ConvocatoriasListarComponent }
 ];
 
 @NgModule({
---
+++ src/app/convocatorias/convocatorias-listar/convocatorias-listar.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-convocatorias-listar',
+  templateUrl: './convocatorias-listar.component.html',
+  styleUrls: ['./convocatorias-listar.component.css']
+})
+export class ConvocatoriasListarComponent implements OnInit {
+
+  carrera: any;
+  ciclo: any;
+
+  constructor() { 
+    }
+
+  ngOnInit() {
+    this.cargar();
+  }
+
+
+  cargar():void{
+
+    this.carrera =[
+      {id:1, nombre:"Ingenieria de software"},
+      {id:2, nombre:"Administracion de negocios"},
+      {id:3, nombre:"Administacion de empresas"},
+      {id:4, nombre:"Ingenieria industrial"},
+      {id:5, nombre:"Ingenieria mecatronica"},
+      {id:6, nombre:"Contaduria publica"},
+      {id:7, nombre:"Publicidad"},
+      {id:8, nombre:"Diseño visual digital"},
+    ];
+
+    this.ciclo =[
+      {id:1, nombre:"Tecnico"},
+      {id:2, nombre:"Tecnologico"},
+      {id:3, nombre:"Profesional"}
+    ]
+
+  }
+
+}
