2d15e15602079e18d1242fff177c6c60963b9b71
Daryl Ospina <daryl.8220@gmail.com>
2019-09-13 23:56:07 -0500

    [resolves #8] form create student request terminated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SolicitudEstudianteComponent } from './solicitudes/solicitud-estudiante/solicitud-estudiante.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'solicitud/estudiante',
+    component: SolicitudEstudianteComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/solicitudes/solicitudes.module.ts
+++ src/app/solicitudes/solicitudes.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SolicitudEstudianteComponent } from './solicitud-estudiante/solicitud-estudiante.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SolicitudEstudianteComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class SolicitudesModule { }
