0fc6055f176d158109802e0d9278fb7d6944037d
Gabriela <gabrielaaa1413@gmail.com>
2019-09-13 21:14:03 -0500

    [resolves #4] Frontend list calls

--- src/app/convocatorias/convocatorias-listar/convocatorias-listar.component.ts
+++ src/app/convocatorias/convocatorias-listar/convocatorias-listar.component.ts
@@ -21,21 +21,21 @@ export class ConvocatoriasListarComponent implements OnInit {
   cargar(): void {
 
     this.carrera = [
-      {id:1 , nombre :' Ingenieria de software '},
-      {id:2 , nombre :' Administracion de negocios '},
-      {id:3 , nombre :' Administacion de empresas '},
-      {id:4 , nombre :' Ingenieria industrial '},
-      {id:5 , nombre :' Ingenieria mecatronica '},
-      {id:6 , nombre :' Contaduria publica '},
-      {id:7 , nombre :' Publicidad '},
-      {id:8 , nombre :' Diseño visual digital '},
+      {id:1 , nombre : ' Ingenieria de software '},
+      {id:2 , nombre : ' Administracion de negocios '},
+      {id:3 , nombre : ' Administacion de empresas '},
+      {id:4 , nombre : ' Ingenieria industrial '},
+      {id:5 , nombre : ' Ingenieria mecatronica '},
+      {id:6 , nombre : ' Contaduria publica '},
+      {id:7 , nombre : ' Publicidad '},
+      {id:8 , nombre : ' Diseño visual digital '},
     ];
 
     this.ciclo = [
-      {id:1, nombre:' Tecnico '},
-      {id:2, nombre:' Tecnologico '},
-      {id:3, nombre:' Profesional '}
-    ]
+      {id:1 , nombre:' Tecnico '},
+      {id:2 , nombre:' Tecnologico '},
+      {id:3 , nombre:' Profesional '}
+    ];
 
   }
 
