106db0a532ea3dfe94b80206a4315d31a9289deb
Gabriela <gabrielaaa1413@gmail.com>
2019-09-13 21:08:20 -0500

    [resolves #4] Frontend list calls

--- src/app/convocatorias/convocatorias-listar/convocatorias-listar.component.ts
+++ src/app/convocatorias/convocatorias-listar/convocatorias-listar.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-convocatorias-listar',
   templateUrl: './convocatorias-listar.component.html',
@@ -20,21 +18,21 @@ export class ConvocatoriasListarComponent implements OnInit {
     this.cargar();
   }
 
-  cargar() :void {
+  cargar():void {
 
-    this.carrera =[
+    this.carrera = [
                  
-      {id:1, nombre: ' Ingenieria de software '},
-      {id:2, nombre:' Administracion de negocios '},
-      {id:3, nombre:' Administacion de empresas '},
-      {id:4, nombre:' Ingenieria industrial '},
-      {id:5, nombre:' Ingenieria mecatronica '},
-      {id:6, nombre:' Contaduria publica '},
-      {id:7, nombre:' Publicidad '},
-      {id:8, nombre:' Diseño visual digital '},
+      {id:1, nombre :' Ingenieria de software '},
+      {id:2, nombre :' Administracion de negocios '},
+      {id:3, nombre :' Administacion de empresas '},
+      {id:4, nombre :' Ingenieria industrial '},
+      {id:5, nombre :' Ingenieria mecatronica '},
+      {id:6, nombre :' Contaduria publica '},
+      {id:7, nombre :' Publicidad '},
+      {id:8, nombre :' Diseño visual digital '},
     ];
 
-    this.ciclo =[
+    this.ciclo = [
       {id:1, nombre:' Tecnico '},
       {id:2, nombre:' Tecnologico '},
       {id:3, nombre:' Profesional '}
