98a9a5b816a33d939d0dd851b2e46ebd13eecaa7
Alejandro Hoyos <0670alejo@gmail.com>
2019-09-14 00:50:44 -0500

    [resolves #6] FRONT_Cerrar convocatoria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,19 +5,21 @@ import { InicioPracticaComponent } from './practicas/inicio-practica/inicio-prac
 import { ConvocatoriasListarComponent } from 'src/app/convocatorias/convocatorias-listar/convocatorias-listar.component';
 import { ConvocatoriasInscripcionComponent } from 'src/app/convocatorias/convocatorias-inscripcion/convocatorias-inscripcion.component';
 import { ConvocatoriasModificarComponent } from 'src/app/convocatorias/convocatorias-modificar/convocatorias-modificar.component';
+import { CerrarConvocatoriaComponent } from './convocatorias/cerrar-convocatoria/cerrar-convocatoria.component';
 
 const routes: Routes = [
-  { path : 'inscripcion', component : ConvocatoriasInscripcionComponent },
-  { path : 'solicitud/estudiante', component : SolicitudEstudianteComponent },
-  { path : 'modificar', component : ConvocatoriasModificarComponent },
-  { path : 'inicio-practica', component : InicioPracticaComponent },
-  { path : 'listar', component : ConvocatoriasListarComponent },
-  { path : 'inicio-practica', component : InicioPracticaComponent },
-  { path : 'listar', component : ConvocatoriasListarComponent }
+  { path: 'inscripcion', component: ConvocatoriasInscripcionComponent },
+  { path: 'solicitud/estudiante', component: SolicitudEstudianteComponent },
+  { path: 'modificar', component: ConvocatoriasModificarComponent },
+  { path: 'inicio-practica', component: InicioPracticaComponent },
+  { path: 'listar', component: ConvocatoriasListarComponent },
+  { path: 'inicio-practica', component: InicioPracticaComponent },
+  { path: 'listar', component: ConvocatoriasListarComponent },
+  { path: 'cierre-convocatoria', component: CerrarConvocatoriaComponent }
 ];
 
 @NgModule({
-  imports : [RouterModule.forRoot(routes)],
-  exports : [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/convocatorias/convocatorias.module.ts
+++ src/app/convocatorias/convocatorias.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ConvocatoriasInscripcionComponent } from './convocatorias-inscripcion/convocatorias-inscripcion.component';
 import { ConvocatoriasModificarComponent } from './convocatorias-modificar/convocatorias-modificar.component';
 import { ConvocatoriasListarComponent } from './convocatorias-listar/convocatorias-listar.component';
+import { CerrarConvocatoriaComponent } from './cerrar-convocatoria/cerrar-convocatoria.component';
 
 @NgModule({
   declarations: [
     ConvocatoriasModificarComponent,
     ConvocatoriasInscripcionComponent,
-    ConvocatoriasListarComponent
+    ConvocatoriasListarComponent,
+    CerrarConvocatoriaComponent
   ],
+  imports: [
+    CommonModule
+  ]
 })
 
 export class ConvocatoriasModule { }
