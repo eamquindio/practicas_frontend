8d746855cb0b8e4e56ba5c91da88e5738a870623
diegolt8 <diegoalugo@hotmail.com>
2019-09-13 20:06:23 -0500

    [resolves #3] FRONT_modify announcement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { SolicitudEstudianteComponent } from './solicitudes/solicitud-estudiante/solicitud-estudiante.component';
 import { InicioPracticaComponent } from './practicas/inicio-practica/inicio-practica.component';
 import { ConvocatoriasInscripcionComponent } from 'src/app/convocatorias/convocatorias-inscripcion/convocatorias-inscripcion.component';
+import { ConvocatoriasModificarComponent } from 'src/app/convocatorias/convocatorias-modificar/convocatorias-modificar.component';
 
 const routes: Routes = [
   { path : 'inscripcion', component : ConvocatoriasInscripcionComponent },
   { path : 'solicitud/estudiante', component : SolicitudEstudianteComponent },
+  { path : 'modificar', component: ConvocatoriasModificarComponent },
   { path : 'inicio-practica', component : InicioPracticaComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports : [RouterModule.forRoot(routes)],
+  exports : [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/convocatorias/entity/facultad.ts
@@ -0,0 +1,6 @@
+export class Facultad{
+
+    public id:Number;
+    public nombre:string;
+
+}
