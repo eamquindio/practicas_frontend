f3a054fc2b2e9788d8af469fdeb70365d2b7bb22
HERNAN MAURICIO VELEZ ROJAS <hernanvelezrojas@gmail.com>
2019-09-14 08:30:38 -0500

    [report_census#9]

--- src/app/practicas/reporte-censo/reporte-censo.component.ts
+++ src/app/practicas/reporte-censo/reporte-censo.component.ts
@@ -7,17 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReporteCensoComponent implements OnInit {
 
-  facultad:any;
+  facultad: any;
   programa: any;
   ciclo: any;
   semestre: any;
   constructor() { }
 
   ngOnInit() {
-    this.facultad=[
-      {id:1, nombre:'Facultad de ingenieria'},
-      {id:2, nombre:'Facultad de Ciencias administrativas y financieras'},
-      {id:3, nombre:'Facultad de Publicidad, Diseño y Comunicacion'}
+    this.facultad = [
+      {id: 1, nombre: 'Facultad de ingenieria'},
+      {id: 2, nombre: 'Facultad de Ciencias administrativas y financieras'},
+      {id: 3, nombre: 'Facultad de Publicidad, Diseño y Comunicacion'}
     ];
     this.programa = [
       {id: 1, nombre : ' Ingenieria de software '},
@@ -35,16 +35,16 @@ export class ReporteCensoComponent implements OnInit {
       {id: 2, nombre: ' Tecnologico '}
     ];
 
-    this.semestre=[
-      {id:1, nombre:'1'},
-      {id:2, nombre:'2'},
-      {id:3, nombre:'3'},
-      {id:4, nombre:'4'},
-      {id:5, nombre:'5'},
-      {id:6, nombre:'6'},
-      {id:7, nombre:'7'},
-      {id:8, nombre:'8'},
-      {id:9, nombre:'9'}
+    this.semestre = [
+      {id: 1, nombre: '1'},
+      {id: 2, nombre: '2'},
+      {id: 3, nombre: '3'},
+      {id: 4, nombre: '4'},
+      {id: 5, nombre: '5'},
+      {id: 6, nombre: '6'},
+      {id: 7, nombre: '7'},
+      {id: 8, nombre: '8'},
+      {id: 9, nombre: '9'}
     ];
   }
 
