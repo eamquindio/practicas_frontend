419c3d5ef5f2756dd39fa811e8fa0f47edb452e7
Alvaro Corrales <alvarito1050@gmail.com>
2019-09-14 01:40:07 -0500

    [resolves #6] FRONT_Registrar inicio practica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SolicitudEstudianteComponent } from './solicitudes/solicitud-estudiante/solicitud-estudiante.component';
+import { InicioPracticaComponent } from './practicas/inicio-practica/inicio-practica.component';
 
 
 const routes: Routes = [
   {
     path: 'solicitud/estudiante',
     component: SolicitudEstudianteComponent
-  }
+  },
+  { path: 'inicio-practica', component: InicioPracticaComponent }
 ];
 
 @NgModule({
