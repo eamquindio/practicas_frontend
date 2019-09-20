cbfcebfe51b15767fbe04fe938b47a9ae2328dae
Cristian Sinisterra <cristiansinisterra@hotmail.com>
2019-09-19 21:36:56 -0500

    [resolves #27] restablecer contrasenia

--- src/app/usuarios/usuarios.module.ts
+++ src/app/usuarios/usuarios.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RestablecerContraseniaComponent } from './restablecer-contrasenia/restablecer-contrasenia.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [RestablecerContraseniaComponent],
   imports: [
     CommonModule
   ]
