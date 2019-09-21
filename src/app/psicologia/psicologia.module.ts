165f8ed013612d8df0efe0da84cbf67e111430b9
HERNAN MAURICIO VELEZ ROJAS <hernanvelezrojas@gmail.com>
2019-09-21 09:26:36 -0500

    dating_list

--- src/app/psicologia/psicologia.module.ts
+++ src/app/psicologia/psicologia.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AsignarCitaComponent } from './asignar-cita/asignar-cita.component';
+import { ListarCitaComponent } from './listar-cita/listar-cita.component';
 
 
 
 @NgModule({
-  declarations: [AsignarCitaComponent],
+  declarations: [AsignarCitaComponent, ListarCitaComponent],
   imports: [
     CommonModule
   ]
