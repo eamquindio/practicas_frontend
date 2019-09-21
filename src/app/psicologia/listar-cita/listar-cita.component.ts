bbd16ea54b1e0302628750fc18530c86cd8b7794
HERNAN MAURICIO VELEZ ROJAS <hernanvelezrojas@gmail.com>
2019-09-21 12:17:53 -0500

    correcion

--- src/app/psicologia/listar-cita/listar-cita.component.ts
+++ src/app/psicologia/listar-cita/listar-cita.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-cita.component.css']
 })
 export class ListarCitaComponent implements OnInit {
-
+  citas: any;
   constructor() { }
-
   ngOnInit() {
-  }
 
+      this.citas = [
+        {id: 1, hora: '12:09', fecha: '02/09/2019', estudiante: 'hernan mauricio velez rojas',
+         programa: 'ingeneria', practicaoconvocatoria: 'practica', estado: 'nulo'},
+         {id: 2, hora: '2:09', fecha: '02/08/2019', estudiante: 'juan david salgado',
+         programa: 'ingeneria', practicaoconvocatoria: 'convocatoria', estado: 'nulo'},
+         {id: 3, hora: '1:09', fecha: '01/06/2019', estudiante: 'alejandro hoyos',
+         programa: 'ingeneria', practicaoconvocatoria: 'convocatoria', estado: 'nulo'}
+      ];
+  }
 }
