22e53c092ef1ca82c6f4f64730816e3964a8c952
Nicolas Davila <nikojako99@gmail.com>
2019-09-21 00:42:35 -0500

    [resolve #26] change path login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
-
-
 @NgModule({
   declarations: [LoginComponent],
   imports: [
