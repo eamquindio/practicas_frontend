78ae098f62c109270a0fd969c29a81ddc89cad5f
Pedro Andres Ruiz Wartski <pepewartski@gmail.com>
2019-10-05 06:11:56 +0200

    [resolves #39] Student self-assessment coding

---
+++ src/app/practicas/autoevaluacion/autoevaluacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autoevaluacion',
+  templateUrl: './autoevaluacion.component.html',
+  styleUrls: ['./autoevaluacion.component.css']
+})
+export class AutoevaluacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
