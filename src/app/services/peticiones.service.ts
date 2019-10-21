ab939e9af3b93ff4a769ca6d8709e1f55ffcddb8
Nicolas Davila <nicolas.davila@yuxiglobal.com>
2019-10-21 16:03:47 -0500

    [resolve #14] creation of the service implementation

---
+++ src/app/services/peticiones.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeticionesService {
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+
+  get(url): Observable<any> {
+    return this.http.get(url,{ observe: 'response' });
+  }
+
+
+  post(url, data): Observable<any> {
+    return this.http.post(url, data,{ observe: 'response' });
+  }
+
+  put(url, data): Observable<any> {
+    return this.http.put(url, data,{ observe: 'response' });
+  }
+
+  delete(url) : Observable<any>{
+    return this.http.delete(url,{ observe: 'response' });
+  }
+}
