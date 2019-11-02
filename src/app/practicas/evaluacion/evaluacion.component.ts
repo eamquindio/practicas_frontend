eaf14299c8cad6e215e73bb5d4037b7b761869df
Alvaro Corrales <alvarito1050@gmail.com>
2019-11-02 10:49:43 -0500

    [resolves #51] register evaluation

--- src/app/practicas/evaluacion/evaluacion.component.ts
+++ src/app/practicas/evaluacion/evaluacion.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PeticionesService } from './../../services/peticiones.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-evaluacion',
@@ -10,6 +11,10 @@ import { PeticionesService } from './../../services/peticiones.service';
 export class EvaluacionComponent implements OnInit {
 
   constructor(private peticion: PeticionesService) { }
+
+
+  /*headers = new HttpHeaders().set
+  ('', ['Access-Control-Allow-Origin', 'Content-Type', 'http://localhost:4200']);*/
 
   evaluation: any = {
     id: 1,
@@ -34,10 +39,25 @@ export class EvaluacionComponent implements OnInit {
     } else {
       this.evaluation.vinculacion_laboral = 'false';
     }
+
     this.peticion.post('http://34.70.190.6/api/evaluacion/evaluation',
-     JSON.stringify(this.evaluation.value)).subscribe( data => {
-      console.log(data);
-     });
+      this.evaluation).subscribe(data => {
+        this.limpiarCampos();
+      });
+  }
+
+  limpiarCampos() {
+    this.evaluation.this.evaluation.id = 1;
+    this.evaluation.id_practica_estudiante = 2;
+    this.evaluation.vinculacion_laboral = '';
+    this.evaluation.nombre_evaluador = '';
+    this.evaluation.comentario_empresa = '';
+    this.evaluation.satisfaccion = 'Seleccione una opcion';
+    this.evaluation.porque_satisfaccion = '';
+    this.evaluation.capacidades = 'Seleccione una opcion';
+    this.evaluation.comportamiento = 'Seleccione una opcion';
+    this.evaluation.actitud_aptitud = 'Seleccione una opcion';
+    this.evaluation.imagen = 'Seleccione una opcion';
   }
 
 }
