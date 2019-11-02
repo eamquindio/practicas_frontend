0c5308fb8f82bbba818220667434059e122119c4
cristian0895 <mangasalucard@hotmail.com>
2019-11-02 11:03:56 -0500

    [resolves #54] modify annoucement

--- src/app/convocatorias/convocatorias-modificar/convocatorias-modificar.component.ts
+++ src/app/convocatorias/convocatorias-modificar/convocatorias-modificar.component.ts
@@ -4,9 +4,10 @@ import { Empresa } from 'src/app/convocatorias/entity/empresa';
 import { Ciclo } from 'src/app/convocatorias/entity/ciclo';
 import { TipoPractica } from 'src/app/convocatorias/entity/tipo-practica';
 import { Programa } from 'src/app/convocatorias/entity/programa';
-
-
-
+import { Estado } from 'src/app/convocatorias/entity/estado';
+import { Coordinador } from 'src/app/convocatorias/entity/coordinador';
+import { NgForm } from '@angular/forms';
+import { PeticionesService } from 'src/app/services/peticiones.service';
 @Component({
   selector: 'app-convocatorias-modificar',
   templateUrl: './convocatorias-modificar.component.html',
@@ -18,12 +19,42 @@ export class ConvocatoriasModificarComponent implements OnInit {
   empresa: Empresa[];
   ciclo: Ciclo[];
   tipoPractica: TipoPractica[];
+  estado: Estado[];
+  coordinador: Coordinador[];
   facultadSelect: Programa[];
   facultadIngeniera: Programa[];
   facultadAdministracion: Programa[];
   facultadDiseno: Programa[];
   sinFacultad: Programa[];
+  annoucement: any = {
+    id: 1,
+    fecha_inicio: '',
+    fecha_final: '',
+    id_empresa: 0,
+    tipo_practica: '-Seleccione-',
+    id_facultad: 0,
+    id_programa: 0,
+    id_ciclo: 0,
+    estado: '-Seleccione-',
+    numero_estudiantes: 0,
+    id_coordinador: 0
+  };
+  constructor(private peticiones: PeticionesService){
 
+  }
+  cargarObjeto():void{
+    this.annoucement.id=1;
+    this.annoucement.fecha_inicio= '';
+    this.annoucement.fecha_final= '';
+    this.annoucement.id_empresa= 0;
+    this.annoucement.tipo_practica='-Seleccione-';
+    this.annoucement.id_facultad= 0;
+    this.annoucement.id_programa= 0;
+    this.annoucement.id_ciclo= 0;
+    this.annoucement.estado= '-Seleccione-';
+    this.annoucement.numero_estudiantes= 0;
+    this.annoucement.id_coordinador= 0;
+  }
   ngOnInit() {
     this.cargar();
     this.cargarProgrmas(0);
@@ -43,6 +74,14 @@ export class ConvocatoriasModificarComponent implements OnInit {
       {id: 1, nombre: 'Safe'},
       {id: 2, nombre: 'Yuxi'},
       {id: 3, nombre: 'Heinsohn'}
+    ];
+    this.coordinador = [
+      {id: 1, nombre: 'Erika'},
+      {id: 2, nombre: 'Martin'}
+    ];
+    this.estado = [
+      {id: 1, nombre: 'Activo'},
+      {id: 2, nombre: 'Inactivo'}
     ];
     this.ciclo = [
       {id: 1, nombre: 'Tecnico'},
@@ -86,4 +125,13 @@ export class ConvocatoriasModificarComponent implements OnInit {
       this.facultadSelect = this.facultadDiseno;
     }
   }
+  addCategory(formNew: NgForm) {
+    this.peticiones.put('http://34.70.190.6/api/convocatoria/convocatorias/'+this.annoucement.id,
+    this.annoucement).subscribe(data=>{
+      console.log(data);
+    });
+    this.cargarObjeto();
+    console.log(this.annoucement);
+  }
 }
+
