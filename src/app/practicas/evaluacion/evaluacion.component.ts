92d4d0fd35f62322d723b22fad045a45df315464
Alvaro Corrales <alvarito1050@gmail.com>
2019-11-02 00:24:14 -0500

    [resolves #51] register evaluation

--- src/app/practicas/evaluacion/evaluacion.component.ts
+++ src/app/practicas/evaluacion/evaluacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-evaluacion',
@@ -9,7 +10,25 @@ export class EvaluacionComponent implements OnInit {
 
   constructor() { }
 
+  evaluation: any = {
+    id: 0,
+    id_practica_estudiante: 1,
+    vinculacion_laboral: '',
+    nombre_evaluador: '',
+    comentario_empresa: '',
+    satisfaccion: '',
+    porque_satisfaccion: '',
+    capacidades: 'Seleccione una opcion',
+    comportamiento: 'Seleccione una opcion',
+    actitud_aptitud: 'Seleccione una opcion',
+    imagen: 'Seleccione una opcion'
+  };
+
   ngOnInit() {
+  }
+
+  addCategory(formNew: NgForm) {
+    console.log(this.evaluation);
   }
 
 }
